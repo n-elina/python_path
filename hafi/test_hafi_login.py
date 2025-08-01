41c69fe9bba03338c14d689d141784a7e9e4d045
Ckogann <ionov.kos@gmail.com>
2025-08-01 18:54:20 +0200

    Login test has been written

--- hafi/test_hafi_login.py
+++ hafi/test_hafi_login.py
@@ -1,9 +1,15 @@
+import pytest
 from selene import browser
 
-def test_valid_login():
+
+@pytest.fixture(scope="module")
+def login():
     browser.open("https://app.hafi.pro/login-v2/")
     browser.element('[placeholder="Enter email"]').type("bemero7907@ikanteri.com")
     browser.element('[placeholder="Enter password"]').type("1234567890").press_enter()
 
-def test_search_in_sidebar():
-    browser.element()
+
+def test_sidebar_search(login):
+    browser.element('//div[text()="Check influencer"]/..').click()
+    browser.element('[placeholder="Enter account name"]').click().type("therock")
+    browser.element('.sidebar-page [href="/profile/instagram/therock"]').click()
